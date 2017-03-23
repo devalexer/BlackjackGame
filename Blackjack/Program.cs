5cb557440a23ff1f6478af488ea74303103c6193
devalexer <alliemckay.dev@gmail.com>
2017-03-23 14:50:26 -0400

    Initial commit with build notes

---
+++ Blackjack/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Define deck(name all cards -aces are 11)
+            //In Suit-Rank.cs
+
+            //Define suits(spades, diamonds, hearts, clubs)
+            //In Suit-Rank.cs
+
+            //Define quantity of cards(52)
+            //Define how deck shuffles(random card)
+            //Define dealer hand
+            //Define player hand
+            //Define dealer rules(hits if they hold less than 16, otherwise Dealer stays)
+            //Identify target as 21
+
+            //Print first dealt card for dealer
+            //Print first and second dealt cards for player
+            //Identify if first and second cards equal 21
+            //If player's first and second card equal 21 automatic win
+            //Otherwise, ask player if they want another card
+            //Read input
+            //Print another card if requested(up to 5 cards max total)
+            //If allowed by dealer rules, deal another card for dealer(up to 5 cards max total)
+            //Each time card is dealt, evaluate if total is 21 or above
+            //If both dealer and player cards equal 21, output draw
+            //If 21, player wins
+            //If above, player loses
+            //If neither card totals equal 21, higher value wins, lower value loses
+        }
+    }
+}
